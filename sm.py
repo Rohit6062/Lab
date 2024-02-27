bdeb886d6079c2a96b0abecd28fbb67a7f0e0ab4
Rohit6062 <rohitranaware0096@gmail.com>
2024-02-27 18:49:53 +0530

    LAb

--- sm.py
+++ sm.py
@@ -1,68 +1,66 @@
 from copy import deepcopy
-def config(r,c,n):
-    global cf
-    for i in range(n):
-        cf[i][c]=False
-        #print(cf)
-    i=0
-    while(i<n):
-        cf[r][i]=False
-        i+=1
-    #print(cf)
-    i=r 
-    j=c
-    #print(cf)
-    while(i<n and j <n):
-            cf[i][j]=False
-            i+=1
-            j+=1
-    i=r
-    j=c
-    while(i<n and j>=0):
-            cf[i][j]=False
-            i+=1
-            j-=1
+tf=[]
+tmp=[]
 
-def f(x,r,n):
-        global cf
+#x=deepcopy(n)
+ans=[]
+class Solution:
+    def chk(r,c,n):
+        global tmp 
+        for i in range(n):
+                if(tmp[i][c]=='Q'):
+                        return False
+        for i in range(n):
+                if(tmp[r][i]=='Q'):
+                        return False
+        i=r
+        j=c
+        while(i>=0 and j>=0):
+                if(tmp[i][j]=='Q'):
+                        return False
+                i-=1
+                j-=1
+        i=r
+        j=c
+        while(i>=0 and j<n):
+                if(tmp[i][j]=='Q'):
+                        return False
+                i-=1
+                j+=1
+        return True
+    def f(x,r,n):
         global tmp
-        global cftmp
-        global ans
+        global ans 
         if(x==0):
                 t=deepcopy(tmp)
                 ans.append(t)
-                return 
+                return
         for i in range(n):
-                if(cf[r][i]==True):
-                        cftmp=deepcopy(cf)
+                if(Solution.chk(r,i,n)):
                         tmp[r][i]='Q'
-                        config(r,i,n)
-                        f(x-1,r+1,n)
+                        Solution.f(x-1,r+1,n)
                         tmp[r][i]='.'
-                        if(r==0):
-                            cf=deepcopy(tmp1)
-                        else:
-                            cf=deepcopy(cftmp)
         return
-
-
-n=input()
-tf=[]
-cf=[]
-tmp=[]
-for i in range(n):
-    for j in range(n):
-        tf.append(True)
-    cf.append(tf)
-    tf=[]
-    for j in range(n):
-        tf.append('.')
-    tmp.append(tf)
-    tf=[]
-cftmp=[]
-tmp1=deepcopy(cf)
-x=deepcopy(n)
-ans=[]
-f(n,0,n)
-for i in ans:
-        print(i)
+    def solveNQueens(n):
+        global tmp
+        global tf
+        global ans
+        for i in range(n):
+            for j in range(n):
+                tf.append('.')
+            tmp.append(tf)
+            tf=[]
+        Solution.f(n,0,n)
+        k=[]
+        s=[]
+        t=''
+        for i in ans: 
+            for j in i:
+                for m in j:
+                    t=t+m
+                s.append(t)
+                t=''    
+            k.append(s)
+            s=[]
+        return k
+print(Solution.solveNQueens(12))
